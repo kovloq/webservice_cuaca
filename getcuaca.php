3fbe83fef3dc3f4548eb92a1f34d0b560f2471e5
kovloq <perdana_adhitama@yahoo.com>
2016-02-04 22:30:38 +0900

    new update

--- getcuaca.php
+++ getcuaca.php
@@ -2,15 +2,23 @@
 include "koneksi.php"; 
 header('Content-Type: application/json');
 $tanggal = date('d F Y');
+$datetime = new DateTime('tomorrow');
+$besok=$datetime->format('d F Y');
 $query="SELECT * FROM cuaca JOIN kota ON kota.id=cuaca.kota_id where kota.name='".$_POST["kota"]."' AND tanggal='".$tanggal."'";
 $result=mysql_query($query);
 $data=mysql_fetch_array($result);
+$query2="SELECT * FROM cuaca JOIN kota ON kota.id=cuaca.kota_id where kota.name='".$_POST["kota"]."' AND tanggal='".$besok."'";
+$result2=mysql_query($query2);
+$data2=mysql_fetch_array($result2);
 header('Content-Type: application/json');
 $arr=array(
 	"name"=>$data["name"],
  	"cuaca"=>$data["cuaca"],
 	"image"=>$data["image"],
-	"tanggal"=>$data["tanggal"]
+	"tanggal"=>$data["tanggal"],
+	"cuaca_besok"=>$data2["cuaca"],
+	"image_besok"=>$data2["image"],
+	"tanggal_besok"=>$data2["tanggal"]
  	);
 echo json_encode($arr);
 ?>
--- today_cuaca.php
+++ today_cuaca.php
@@ -6,7 +6,6 @@ ini_set('display_errors', 1);
 //Loop From User ID 1 to 5
 $query="SELECT * FROM propinsi WHERE id between ".$_GET["from"]." AND ".$_GET["to"];
 $result=mysql_query($query);
-
 while($data = mysql_fetch_array($result)){
 	$url=$data["today_url"];
 	$xml=simplexml_load_file($url);
@@ -22,15 +21,10 @@ while($data = mysql_fetch_array($result)){
 		}else{
 			$name=$xml->Isi->Row[$i]->Kota;
 		}
-		$query2="SELECT * FROM kota WHERE name='".$name."'";
+		$query2="SELECT * FROM kota WHERE name='".addslashes($name)."'";
 		$result2=mysql_query($query2);
 		$data2=mysql_fetch_array($result2);
 		$kota_id=$data2["id"];
-		// // Compare Tomorrow URL and today URL
-		$query3="SELECT * FROM cuaca WHERE tanggal='".$tanggal."' AND kota_id='".$kota_id."'";
-		$result3=mysql_query($query3);
-		$data3=mysql_fetch_array($result3);
-		$cuaca_besok=$data3["cuaca"];
 		if($id==12){
 			$pagi=$xml->Isi->Row[$i]->Pagi;
 			$siang=$xml->Isi->Row[$i]->Siang;
@@ -40,26 +34,18 @@ while($data = mysql_fetch_array($result)){
 			$siang_image=str_replace(" ","_",strtolower($siang));
 			$malam_image=str_replace(" ","_",strtolower($malam));
 			$image=$pagi_image."__".$siang_image."__".$malam_image;
-			if($cuaca_besok!=$cuaca && strlen($cuaca_besok > 9)){
-				$cuaca=$cuaca_besok;
-			}
 		}else{
 			$cuaca=$xml->Isi->Row[$i]->Cuaca;
-			$image=str_replace(" ","_",strtolower($cuaca));
-			// Compare Tomorrow URL and Today URL
-			if($cuaca_besok=="-" && $cuaca!="-"){
-				$query="UPDATE cuaca set cuaca='".$cuaca."' WHERE tanggal='".$tanggal."' AND kota_id='".$kota_id."'";
-				mysql_query($query);//echo "satu";
-			}elseif(trim($cuaca_besok)!=trim($cuaca)){
-				$query="UPDATE cuaca set cuaca='".$cuaca."' WHERE tanggal='".$tanggal."' AND kota_id='".$kota_id."'";
-				mysql_query($query);//echo "dua";
-			}elseif($cuaca_besok=="-" AND $cuaca="-"){
-				$query="UPDATE cuaca set cuaca='Tidak ada data',image='no_data.png' WHERE tanggal='".$tanggal."' AND kota_id='".$kota_id."'";
-				mysql_query($query);//echo "tiga";
+			if($cuaca=="-"){
+				$cuaca="Tidak ada data";
 			}
+			$image=str_replace(" ","_",strtolower($cuaca));
 		}
 		
-		
+		$query="INSERT INTO cuaca (propinsi_id,kota_id,tanggal,cuaca,image) VALUES ('".$id."','".$kota_id."','".$tanggal."','".$cuaca."','".$image."')";
+		// echo "<br/>";
+		mysql_query($query);
 	}
 }
+echo "OK";
 ?>
